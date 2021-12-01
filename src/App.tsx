38b31cf77d762a51b4981b0580eee646c1cc70bf
parvathig2 <parvathig2811@gmail.com>
2021-12-01 14:21:20 +0530

    firstcommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+      
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+
+  },
+  {
+    title:"redux",
+    url: "http://redux.js.org/",
+    author: "Dan abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function getTitle(){
+  return "Hello React"
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container">
+      <h1>{getTitle()}</h1>
+      <label htmlFor="search">search</label>
+      <input id="search"/>
+      <div className="tableHeader">
+      <span>Title</span>
+          <span>URL</span>
+          <span>Author</span>
+      </div>
+
+      {list.map((item) => (
+        <div>
+          <span>{item.title}</span>
+          <span>{item.url}</span>
+          <span>{item.author}</span>
+          </div>
+      ))}
     </div>
   );
 }
-
 export default App;
